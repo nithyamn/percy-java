d54412022d9482a11ed619e164d20269cc14f034
nithyamani <nithya.mn999@gmail.com>
2020-06-19 18:50:26 +0530

    changed css

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -9,13 +9,15 @@ public class TestCase extends Runner{
     @Test
     public void test() throws InterruptedException {
         Percy percy = new Percy(driver);
-        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
+        //driver.manage().timeouts().implicitlyWait(10000, TimeUnit.SECONDS);
 
         driver.get("http://localhost:8001/");
-        percy.snapshot("About Us");
+        Thread.sleep(3000);
+        percy.snapshot("Percy - About Us");
 
         //Thread.sleep(2000);
         driver.findElement(By.linkText("Contact Us!")).click();
-        percy.snapshot("Contact Us");
+        Thread.sleep(3000);
+        percy.snapshot("Percy - Contact Us");
     }
 }
